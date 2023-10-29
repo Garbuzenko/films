346c2f8478d61ee67a1e167bf2bbeafcf240c0f9
Garbuzenko <mike.garbuzenko@gmail.com>
2023-10-29 20:34:46 +0300

    ok

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import "./App.css";
 import Stars from "./components/Stars";
 
 function App() {
-  return <Stars count={1} />;
+  return <>
+   <Stars count={1} />
+   <Stars count={2} />
+   <Stars count={3} />
+   <Stars count={4} />
+   <Stars count={5} />
+  </>
+ ;
 }
 
 export default App;
